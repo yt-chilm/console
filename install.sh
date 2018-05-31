2388b96f95911e6d5151bed3cd4201ecc3d6008a
chilm <chilled.coder@gmail.com>
2018-05-31 15:16:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,6 @@ sudo apt-get install nano -y
 sudo apt-get install gcc -y
 sudo apt-get install make -y
 sudo apt-get install libncurses5-dev libncursesw5-dev -y
+
+sudo git config --global user.email "chilled.coder@gmail.com"
+sudo git config --global user.name "yt-chilm"
