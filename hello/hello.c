71a3d5d557f335b64faaf8f8158a65252e12ec2e
yt-chilm <chilled.coder@gmail.com>
2018-05-31 13:43:11 +0000

    updated with colour

--- hello/hello.c
+++ hello/hello.c
@@ -7,14 +7,24 @@ int main(int argc, char *argv[]) {
   noecho();
   curs_set(FALSE);
 
+  start_color();
+
   while(1) {
     clear();
+
+    if(can_change_color()) {
+      init_color(COLOR_RED, rand()%1000, rand()%1000, rand()%1000);
+    }
+
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
+    attrset(COLOR_PAIR(1));
 
     mvprintw(rand()%40,rand()%100,"Hello, world!");
 
     refresh();
 
-    usleep(100000);
+    usleep(250000);
   }
 
   endwin();
